4ed2b86dc904a68863a732beb273d1779538bc01
claguerre <contact@idweb-agence.com>
2017-05-10 17:01:25 +0200

    update languages files

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wpplugin2017');
+define('DB_NAME', 'expense-manager-grp1');
 
 /** MySQL database username */
 define('DB_USER', 'root');
